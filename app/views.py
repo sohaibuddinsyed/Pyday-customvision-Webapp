2adff1823de775f681e00617e0fa543a96939d7b
sohaib <coolsohaib602@gmail.com>
2020-07-04 16:18:38 +0530

    local file upload added

--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
 # views.py
 
-from flask import render_template
-from msrest.authentication import ApiKeyCredentials
-import time
+from flask import *
+import os
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 from msrest.authentication import ApiKeyCredentials
+
+prediction_key = "21dcf849cf844333a0c1f45f6e5937aa"
+publish_iteration_name = "classifyModel"
+ENDPOINT = "https://testcvp.cognitiveservices.azure.com/"
+projectId = "dc248edb-8a7d-4314-90a7-0494403301a3"
+
 
 from app import app
 
@@ -14,28 +19,23 @@ def home():
     return render_template("home.html")
 
 
-prediction_key = "21dcf849cf844333a0c1f45f6e5937aa"
-prediction_resource_id = "/subscriptions/4199da6c-8f1e-4bfb-a8c1-62ba5bc76d73/resourceGroups/AzurePy/providers/Microsoft.CognitiveServices/accounts/TestCVP-Prediction"
-
-
 @app.route('/success', methods = ['POST'])  
 def success():  
     if request.method == 'POST':  
         f = request.files['file']
-        # # Now there is a trained endpoint that can be used to make a prediction
-        # prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
-        # predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
+        f.save(os.path.join(app.root_path, "static","test.jpg")) 
 
-        # with open(base_image_url + "withmask/testImg.jpg", "rb") as image_contents:
-        #     results = predictor.classify_image(
-        #     project.id, publish_iteration_name, image_contents.read())
+        prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": prediction_key})
+        predictor = CustomVisionPredictionClient(ENDPOINT, prediction_credentials)
 
-        # # Display the results.
-        # for prediction in results.predictions:
-        #     print("\t" + prediction.tag_name +
-        #           ": {0:.2f}%".format(prediction.probability * 100))
-        f.save(f.filename)  
-        return render_template("success.html", name = f.filename)  
+        with open(app.root_path + "/static/test.jpg", "rb") as image_contents:
+            results = predictor.classify_image(
+                projectId, publish_iteration_name, image_contents.read())
+            result=""
+            for prediction in results.predictions:
+                result += "\t" + prediction.tag_name + ": {0:.2f}%".format(prediction.probability * 100)
+
+        return render_template("success.html", result = result)  
 
 
 @app.route('/demo')
