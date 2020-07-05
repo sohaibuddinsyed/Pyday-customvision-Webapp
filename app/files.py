7afaddc6bcb5ed3530de8072d41662d9ff8bfbcd
Syed Sohaib Uddin <coolsohaib602@gmail.com>
2020-07-05 23:54:23 +0530

    Code made generic for accessibility

--- app/files.py
+++ app/files.py
@@ -5,7 +5,7 @@ from msrest.authentication import ApiKeyCredentials
 from app import app
 
 
-prediction_key = "21dcf849cf844333a0c1f45f6e5937aa"
+prediction_key = "your-prediction key"
 publish_iteration_name = "classifyModel"
-ENDPOINT = "https://testcvp.cognitiveservices.azure.com/"
-projectId = "dc248edb-8a7d-4314-90a7-0494403301a3"
+ENDPOINT = "your-endpoint"
+projectId = "your project id"
