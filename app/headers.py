37b69e54bfcc90c8d334af46ca7252ac51bdf04f
Azhar Ali <azhar042014@gmail.com>
2020-07-07 13:04:18 +0530

    Key change

--- app/headers.py
+++ app/headers.py
@@ -9,7 +9,7 @@ from app import app
 prediction_key = "21dcf849cf844333a0c1f45f6e5937aa"
 publish_iteration_name = "classifyModel"
 ENDPOINT = "https://testcvp.cognitiveservices.azure.com/"
-projectId = "dc248edb-8a7d-4314-90a7-0494403301a3"
+projectId = "e9d53526-fb02-4522-8c89-74d65fa3a624"
 # prediction_key = "your-prediction key"
 # publish_iteration_name = "classifyModel"
 # ENDPOINT = "your-endpoint"
